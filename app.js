6f8101484bc4c2c7104fac43f2ee6c339605721d
Samrat Saha <samratsaha9635@gmail.com>
2025-12-11 23:30:31 +0530

    Integrate auth, user, and subscription route modules into API v1

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ import express from 'express';
 
 import { PORT } from './config/env.js';
 
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+
 const app = express();
 
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+
 app.get('/', (req, res) =>{
-    res.send("Hello This is Subtrack server");
+    res.send("Welcome to the SubSphere API!");
 });
 
 app.listen(PORT, () =>{
