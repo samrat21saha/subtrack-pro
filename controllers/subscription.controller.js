9521cc0636ddcc9b544111e7da2f5805ebe30884
Samrat Saha <samratsaha9635@gmail.com>
2025-12-18 22:05:17 +0530

    Add subscription controller for creating subscriptions and fetching user-scoped records

---
+++ controllers/subscription.controller.js
@@ -0,0 +1,33 @@
+import Subscription from "../models/subscription.model";
+
+export const createSusbscription = async (req, res, next) =>{
+    try{
+        const subscription = await Subscription.create({
+            ...req.body,
+            user: req.user._id,
+        });
+
+
+        res.status(201).json({success: true, data: subscription});
+    }catch(error){
+        next(error);
+    }
+}
+
+
+export const getUserSubscriptions = async(req, res, next) =>{
+    try{
+        if(req.user.id != req.prams.id){
+            const error = new Error('You are not owner of this account');
+            error.status = 401;
+            throw error;
+        }
+        const subscriptions = Subscription.find({user: req.params.id});
+        res.status(200).json({success: true, data: subscriptions});
+    }catch(error){
+        next(error);
+    }
+}
+
+
+export default createSusbscription;
